77a8d1e8fc7c54901ab64d93cf6f52b037197bf8
val <sebsmith237@gmail.com>
2026-08-05 13:20:29 +1200

    tictac

--- lesson16.py
+++ lesson16.py
@@ -1,46 +1,64 @@
-"""
-def slice(x):
-    return x.lower().split("@")
-"""
-
-"""
-import random
-
-def generate_number():
-    return random.randint(0,100)
-
-def get_guess():
-    return round(float(input("Choose a number: ")))
-
-def check_guess(n, g):
-    return "more" if n > g else "less" if g > n else True
-
-def main():
-    n = generate_number()
-    while True:
-        r = check_guess(n, get_guess())
-        print("Higher!" if r == "more" else "Lower!" if r == "less" else "Correct!")
-        if r == True: break
-
-main()
-"""
+from colorama import Fore, Back, Style; from pynput import keyboard
 
 board = [0,0,0,0,0,0,0,0,0]
+turn = 1
 
-def print_board(b):
+def clear():
+    print("\033[H\033[J", end="")
+
+def player(n, t = True):
+    return f"{Fore.RED}{Style.BRIGHT}X" + (Style.RESET_ALL if t else "") if n == 1 else f"{Fore.BLUE}{Style.BRIGHT}O" + (Style.RESET_ALL if t else "")
+
+def print_board(b, e=False):
     c = 0
+    global turn
     for i in b:
         c += 1
-        print(" " + str(i), end=" |" if c%3 else "\n")
+        print(" " + ((Style.DIM + str(c) + Style.RESET_ALL) if i == 0 else player(i)), end=" |" if c%3 else "\n")
         if not c % 3 and not c == 9: print("---+---+---")
+    if e: print(f"\n{player(turn, False)}'s turn.{Style.RESET_ALL}\nChoose a position: ", end="")
 
 def check_winner(b):
+    full = True
+    win = 0
+    for i in b:
+        if i == 0: full = False
+        else:
+            pass # TODO STUFF HERE
+    
+    if full:
+        clear()
+        print_board(b)
+    
+    return win
+
+def on_press(key):
     pass
 
-def move(b, p):
-    pass
+def on_release(key):
+    global turn
+    if key == keyboard.Key.esc:
+        clear()
+        return False
 
-def main():
-    pass
+    try:
+        pos = int(key.char)-1
+        if board[pos] == 0:
+            board[pos] = turn
+            turn = 1 if turn == 2 else 2
+        win = check_winner(board)
+        if win == 0: clear(); print_board(board, True)
+        else:
+            clear()
+            print_board(board)
+            print(f"\n{player(turn, False)} wins!" if win != 3 else "\nDraw!")
+            return False
+    except Exception as e:
+        clear(); print_board(board, True)
+        return
 
-print_board(board)
+clear()
+print_board(board, True)
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
