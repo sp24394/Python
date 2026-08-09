61351ffb6fef85894b8a26bcd182196cfe903388
val <sebsmith237@gmail.com>
2026-08-10 09:56:54 +1200

    wd

--- clicker/main.py
+++ clicker/main.py
@@ -5,17 +5,15 @@ def logo():
     scriptPath = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(scriptPath, "logo.txt")
     with open(path, "r") as file:
-        return file.read().split("\n&&\n")
+        return file.read()
 
 def clear():
     print("\033[H\033[J", end="")
 
-def start():
-    anim = logo()
-    for i in anim:
-        clear()
-        print(i)
-        time.sleep(0.2/len(anim))
+def start(splash, showmenu):
+    clear()
+    if splash: print(splash)
+    if showmenu:
+        print("  > play\n    quit")
 
-
-start()
+start(logo(), True)
