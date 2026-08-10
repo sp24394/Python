a4eb7e1c422f6e6db9c317d1dd1c9013ce005945
val <sebsmith237@gmail.com>
2026-08-10 16:52:53 +1200

    wd

--- clicker/main.py
+++ clicker/main.py
@@ -1,9 +1,42 @@
 # idle game or something
-import os, time, sys
+import os, time, sys, json; from datetime import datetime, date; from pathlib import Path
+
+scriptPath = os.path.dirname(os.path.abspath(__file__))
+savepath = Path(scriptPath) / "saves"
+
+save = "testsave"
+
+def savefiles():
+    return [p for p in savepath.glob("*.json") if p.is_file()]
+
+def savesmenu():
+    entries = savefiles()
+    entries.append("create a new save +")
+    chosen = menu(entries, " choose a save file +~\n")
+    if chosen != "create a new save +":
+        save = chosen
+    else:
+        success = False
+        clear()
+        inp = input(" name your save slot +~\n  ▌↲ save name: ")
+        if inp.isalnum(): success = True
+        while not success:
+            clear()
+            inp = input(" alpha-numeric characters only +~\n  ▌↲ save name: ")
+            if inp.isalnum(): success = True
+        clear()
+
+def readsave(save):
+    with open(Path(savepath) / f"{save}.json", "r") as save:
+        return json.load(save)
+
+def writesave(save, newdata):
+    savedata = readsave().update(newdata)
+    with open(Path(savepath) / f"{save}.json", "w") as save:
+        json.dump(savedata, save, indent=2)
 
 def logo():
-    scriptPath = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(scriptPath, "logo.txt")
+    path = Path(scriptPath) / "logo.txt"
     with open(path, "r") as file:
         return file.read()
 
@@ -12,6 +45,8 @@ def getchar():
     if os.name == "nt": #means its windows
         import msvcrt
         ch = msvcrt.getch()
+        if ch == b"\r":
+            return "ENTER"
         if ch in (b"\x00", b"\xe0"): #esc keu thingy
             code = msvcrt.getch()
             return {b"H": "UP", b"P": "DOWN", b"K": "LEFT", b"M": "RIGHT"}.get(code, "")
@@ -34,25 +69,33 @@ def getchar():
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old)
         return {"\x1b": "ESC", "\x1b[A": "UP", "\x1b[B": "DOWN",
-        "\x1b[C": "RIGHT", "\x1b[D": "LEFT"}.get(char, char)
+        "\x1b[C": "RIGHT", "\x1b[D": "LEFT", "\r": "ENTER"}.get(char, char)
 
 def clear():
     print("\033[H\033[J", end="")
 
-def start(splash):
-    entries = ["play", "options", "quit"]
+def menu(entries, prefix="", suffix=""):
     selected = 0
     while True:
         clear()
-        if splash: print(splash)
+        print(prefix, end="")
         for i in entries:
-            print(f"  {"▐" if selected == entries.index(i) else " "} {i}")
-        match getchar().lower():
-            case "up" | "w": selected -= 1 if selected > 0 else 0
-            case "down" | "s": selected += 1 if selected < len(entries) - 1 else 0
+            print(f"  {"▌↲" if selected == entries.index(i) else " "} {i}")
+        print(suffix, end="")
+        character = getchar()
+        match character.lower():
+            case "up" | "w":
+                if selected > 0: selected -= 1
+                else: selected = len(entries) - 1
+            case "down" | "s":
+                if selected < len(entries) - 1: selected += 1
+                else: selected = 0
+            case "enter":
+                return entries[selected]
             case _: pass
-                
 
-start(logo())
-while True:
-    print(f"pressed: {getchar()}")
+def splash(icon):
+    clear(); print(icon)
+
+clear()
+savesmenu()
